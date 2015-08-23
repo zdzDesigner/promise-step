de24f47bf8021252bea0ed4adb2be46874207dbf
zdz <1696498456@qq.com>
2015-08-23 15:06:02 +0800

    初步链式

--- promise-then-chained.js
+++ promise-then-chained.js
@@ -3,4 +3,24 @@
  * 实现promise的链式调用
  */
  
- 
+ var ref = function(value) {
+    if(value && value.then === 'function') return value;
+    return {
+        then:function(callback) {
+            return ref(callback(value));
+        }
+    }
+};
+
+
+var promise = ref("step1");
+
+promise.then(function(value){
+    console.log(value); // "step1"
+    return 15;
+}).then(function(value){
+    console.log(value); // 15
+    return 'zdz'
+}).then(function(value){a
+    console.log(value); // zdz
+});
